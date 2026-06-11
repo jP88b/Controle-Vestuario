9a945ccd214d8a90ce10c56ee7cece4e6757f706
João Paulo de Almeida Biló <joaopaulodealmeidabilo@gmail.com>
2026-06-11 20:10:15 -0300

    Delete inter4sem directory

--- inter4sem/controlevestuario/src/main/java/com/inter4sem/controlevestuario/repository/HistoricoSistemaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inter4sem.controlevestuario.repository;
-
-import com.inter4sem.controlevestuario.entity.HistoricoSistemaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HistoricoSistemaRepository extends JpaRepository<HistoricoSistemaEntity, Long> {
-}
